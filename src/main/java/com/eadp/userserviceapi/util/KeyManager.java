818f9f97fca6d8653c79e2e5ed5ccce983528c8c
Helanka Singhapurage <helankas26@gmail.com>
2024-02-06 22:52:52 +0530

    update KeyManager

--- src/main/java/com/eadp/userserviceapi/util/KeyManager.java
+++ src/main/java/com/eadp/userserviceapi/util/KeyManager.java
@@ -1,11 +1,15 @@
 package com.eadp.userserviceapi.util;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+@Component
 public class KeyManager {
     private final String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
 
     public String generateKey(int length) {
+        length = length == 0 ? 1 : length;
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < length; i++) {
             stringBuilder.append(CHARS.charAt(new Random().nextInt(62)));

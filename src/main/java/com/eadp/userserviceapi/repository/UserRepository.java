1a74ba00e713f95eded11c2506d2e2fdcc81953e
Helanka Singhapurage <helankas26@gmail.com>
2024-02-06 21:22:58 +0530

    add user repository

---
+++ src/main/java/com/eadp/userserviceapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eadp.userserviceapi.repository;
+
+import com.eadp.userserviceapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

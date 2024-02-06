7e51710e2fc1062163b0070a4ce839d1e8a89790
Helanka Singhapurage <helankas26@gmail.com>
2024-02-06 20:40:17 +0530

    update user controller with DTO

--- src/main/java/com/eadp/userserviceapi/controller/UserController.java
+++ src/main/java/com/eadp/userserviceapi/controller/UserController.java
@@ -1,33 +1,34 @@
 package com.eadp.userserviceapi.controller;
 
+import com.eadp.userserviceapi.dto.request.RequestUserDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
 
-    @GetMapping
-    public String findAllUsers() {
+    @GetMapping(params = {"page", "size", "searchText"})
+    public String findAllUsers(@RequestParam int page, @RequestParam int size, @RequestParam String searchText) {
         return "findAllUsers";
     }
 
     @PostMapping
-    public String createUser() {
-        return "createUser";
+    public String createUser(@RequestBody RequestUserDto dto) {
+        return dto.toString();
     }
 
-    @GetMapping
-    public String findUser() {
+    @GetMapping("/{id}")
+    public String findUser(@PathVariable String id) {
         return "findUser";
     }
 
-    @PutMapping
-    public String updateUser() {
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody RequestUserDto dto) {
         return "updateUser";
     }
 
-    @DeleteMapping
-    public String deleteUser() {
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id) {
         return "deleteUser";
     }
 }

dffc05bf911cd68af305affbcb00d95320bf2da1
Helanka Singhapurage <helankas26@gmail.com>
2024-02-06 21:16:48 +0530

    add user response, paginate DTO

---
+++ src/main/java/com/eadp/userserviceapi/dto/response/ResponseUserDto.java
@@ -0,0 +1,16 @@
+package com.eadp.userserviceapi.dto.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ResponseUserDto {
+    private String id;
+    private String fullName;
+    private String email;
+    private String avatarUrl;
+    private boolean status;
+}

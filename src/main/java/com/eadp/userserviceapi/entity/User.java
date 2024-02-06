d0937516ce39e1a4416c72444b5ba51b89fb2d0a
Helanka Singhapurage <helankas26@gmail.com>
2024-02-06 21:22:44 +0530

    add user entity

---
+++ src/main/java/com/eadp/userserviceapi/entity/User.java
@@ -0,0 +1,17 @@
+package com.eadp.userserviceapi.entity;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+    private Long propertyId;
+    private String userId;
+    private String fullName;
+    private String email;
+    private String avatarUrl;
+    private boolean status;
+}
